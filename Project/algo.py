5b434d7e7f6bea767cb024524d1f538b641e3bc0
Samuel Schapiro <sjs17@illinois.edu>
2021-09-26 18:20:39 -0500

    Started working on calculating the scores.

---
+++ Project/algo.py
@@ -0,0 +1,15 @@
+def calculateScore(responses):
+
+    openness_score = 0
+    conscientiousness_score = 0
+    extroversion_score = 0
+    agreeableness_score = 0
+    neuroticism_score = 0
+
+    for n in range(0, len(responses)):
+        tuple = responses[n]
+        dimension = tuple[0]
+    return sum
+
+print(calculateScore([(1, 2), (2, 4)]))
+
