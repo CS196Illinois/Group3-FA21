509ee5c5c06c501cbd4742968a88b48d80d0dd57
Aditya Matiwala <adi.matiwala@gmail.com>
2021-11-14 11:10:29 -0600

    Updated db.py

--- api/db.py
+++ api/db.py
@@ -1,10 +1,48 @@
+import mysql.connector
+import match
+
+class User:
+    def __init__(self, user_id, username, password, email, dob, last_active, last_updated):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.dob = dob
+        self.last_active = last_active
+        self.last_updated = last_updated
+
+mydb = None
+
+def createConnection():
+    global mydb
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "password",
+        database = "CS196db"
+    )
+    print(mydb)
+
+
 def get_users():
     """
     Connect to SQL server and return all users
 
     return a list of all users as the user object.
     """
-    return []
+    
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "SELECT * FROM users"
+    cursor.execute(query)
+    user_rows = cursor.fetchall()
+    user_list = []
+
+    for row in user_rows:
+        user_list.append(User(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+
+    return user_list
 
 
 def get_user(user_id = None, username = None, email = None):
@@ -13,7 +51,16 @@ def get_user(user_id = None, username = None, email = None):
 
     Return the user object.
     """
-    return {}
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "SELECT * FROM users WHERE user_id = %s OR email = %s OR username = %s"
+    cursor.execute(query, (user_id, email, username))
+    user_row = cursor.fetchall()
+    user_object = User(user_row[0][0], user_row[0][1], user_row[0][2], user_row[0][3], user_row[0][4], user_row[0][5], user_row[0][6])
+
+    return user_object
+
 
 def update_user(user, user_id = None, username = None, email = None):
     """
@@ -21,6 +68,18 @@ def update_user(user, user_id = None, username = None, email = None):
 
     Return True or False based on the success or failure of the operation
     """
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "UPDATE users SET user_id = %s, username = %s, password = %s, email = %s, dob = %s, last_active = %s, last_updated = %s WHERE user_id = %s OR username = %s OR email = %s"
+
+    try:
+        cursor.execute(query, (user.user_id, user.username, user.password, user.email, user.dob, user.last_active, user.last_updated, user_id, username, email))
+        mydb.commit()
+    except Exception as e:
+        print('Error: ', e)
+        return False
+
     return True
 
 def delete_user(user_id = None, username = None, email = None):
@@ -29,6 +88,18 @@ def delete_user(user_id = None, username = None, email = None):
 
     Return True or False based on the success or failure of the operation
     """
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "DELETE FROM users WHERE user_id = %s OR email = %s OR username = %s"
+    try:
+        cursor.execute(query, (user_id, email, username))
+    except Exception as e:
+        print("Error, ", e)
+        return False
+    
+    mydb.commit()
+
     return True
 
 def add_user(user):
@@ -37,6 +108,16 @@ def add_user(user):
 
     Return True or False based on the success or failure of the operation
     """
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "INSERT INTO users (user_id, username, password, email, dob, last_active) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    try:
+        cursor.execute(query, (user.user_id, user.username, user.password, user.email, user.dob, user.last_active, user.last_updated))
+    except Exception as e:
+        print("Error, ", e)
+        return False
+
     return True
 
 
@@ -44,7 +125,16 @@ def read_questions():
     """
     Return a list of questions
     """
-    return []
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "SELECT question FROM question_text"
+    cursor.execute(query)
+    question_data = cursor.fetchall()
+    question_list = []
+    for row in question_data:
+        question_list.append(row[0])
+    return question_list
 
 
 def add_questions(question):
@@ -57,10 +147,22 @@ def add_questions(question):
 
 def delete_question(qid = None, qtxt = None):
     """
-    Connect to SQL server and delete the user. Either the question id or question text will be provided.
+    Connect to SQL server and delete the question. Either the question id or question text will be provided.
 
     Return True or False based on the success or failure of the operation
     """
+    createConnection()
+    global mydb
+    cursor = mydb.cursor()
+    query = "DELETE FROM question_text WHERE question_id = %s or question = %s"
+    try:
+        cursor.execute(query, (qid, qtxt))
+    except Exception as e:
+        print("Error, ", e)
+        return False
+    
+    mydb.commit()
+
     return True
 
 def run_ranker():
@@ -69,3 +171,6 @@ def run_ranker():
 
     Returns True or False based on the success or failure of the operation.
     """
+
+
+    
